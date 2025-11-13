79087d14dc3bceb18fd78807530034cfe7da7d00
menggf <menggf@gmail.com>
2025-11-13 17:22:43 +0800

    Refactor argument parsing and update RAG initialization

--- main.py
+++ main.py
@@ -6,23 +6,41 @@ import argparse
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="FastBioMedRAG is relationship-based Retrieval-Augmented Generation tool for deep analysis of biological papers.")
-    parser.add_argument('--job', type=str, required=True, help="'either update' or 'query': to add new document or query", default='update')
-    parser.add_argument('--document', type=str,  help="csv file name, it should have at least three columns: abstract, main_text, paper_id.  'main_text' is supposed to be a list[str]. If not, it will be splited by '\n' ", default="./example/demo.csv")
+    parser.add_argument('--job', type=str, required=True, choices=['update', 'query'], help="'either update' or 'query': to add new document or query", default='update')
+    parser.add_argument('--document', type=str,  help="csv file name, it should have at least three columns: abstract, main_text, paper_id.  'main_text' is supposed to be a list[str]. If not, it will be splited by '\n' ")
     parser.add_argument('--question', type=str,  help="The question for query", default="Which genes are associated with endometriosis?")
     parser.add_argument('--working_dir', type=str,  help="The directory to store database", default='./test')
     parser.add_argument('--collection_name', type=str, help="The database name", default='paper')
-    parser.add_argument('--llm_model_name', type=str,help="The ollama LLM model ", default='phi4:latest')
+    parser.add_argument('--llm_update_model_name', type=str,help="The ollama LLM model ", default='phi4:latest')
+    parser.add_argument('--llm_query_model_name', type=str,help="The ollama LLM model ", default='gemma3:27b-it-qat')
     parser.add_argument('--embed_model_name', type=str, help="The ollama embedding model", default='mxbai-embed-large:latest')
     parser.add_argument('--embedding_smilarity', type=float,  help="The embedding similiarity cutoff", default=0.8)
     parser.add_argument('--top_match', type=int, help="The record number returned for query", default=20)
 
     args=parser.parse_args()
     
-    rag=fastbmrag.RAG(working_dir=args.working_dir, 
+ 
+    # Use the appropriate model based on job
+    if args.job == 'update':
+        rag=fastbmrag.RAG(working_dir=args.working_dir, 
                    collection_name=args.collection_name, 
-                   llm_model_name=args.llm_model_name, 
+                   llm_index_model_name=args.llm_update_model_name,
                    embed_model_name=args.embed_model_name, 
                    embedding_smilarity=args.embedding_smilarity)
+    else: 
+        rag=fastbmrag.RAG(working_dir=args.working_dir, 
+                   collection_name=args.collection_name, 
+                   llm_query_model_name=args.llm_query_model_name,
+                   embed_model_name=args.embed_model_name, 
+                   embedding_smilarity=args.embedding_smilarity)
+
+
+
+    #rag=fastbmrag.RAG(working_dir=args.working_dir, 
+                   #collection_name=args.collection_name, 
+                   #llm_model_name=model_name,
+                   #embed_model_name=args.embed_model_name, 
+                   #embedding_smilarity=args.embedding_smilarity)
                    
     if args.job=='update':
         input_text = pd.read_csv(args.document, converters={'main_text': literal_eval})
@@ -36,12 +54,18 @@ if __name__ == '__main__':
         #print(input_text.columns)
         meta_info = input_text.to_dict(orient='records')
         #print(meta_info[0])
-        documents=[ {'abstract': abstract[i], 'main_text': main_text[i], 'meta_info': meta_info[i] } for i in range(len(abstract))]
+
+        documents=[ {'abstract': abstract[i], 'main_text': main_text[i], 'meta_info': meta_info[i], 'paper_id': paper_id[i]} for i in range(len(abstract))]
         #print(documents[0])
-        rag.insert_paper(documents)
+        
+        #convert into DataFrame
+        documents_df = pd.DataFrame(documents)
+
+        rag.insert_paper(documents_df)
         print("Done")
     
     if args.job=='query':
         out=rag.query(args.question)
         print(out)
-    
+
+
